1fdb056f5e2149fcc563d920e6c5844fa301b7b7
Sidqiharhinza26 <83409617+Sidqiharhinza26@users.noreply.github.com>
2022-10-29 20:38:21 +0700

    Delete tugas1 directory

--- tugas1/no1.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  String nama = 'Muhamad Sidqih Arhinza Juliantara';
-  int umur = 20;
-  double ipk = 4.5;
-
-  print('Nama : ' + nama);
-  print('Umur : ' + (umur).toString());
-  print('IPK  : ' + (ipk).toString());
-}
